4c3d74da1420ed45819dbe9a018a80e39d88cd97
Gavin Stark <gavin@gstark.com>
2017-08-09 11:47:26 -0400

    Added /search handler to process the search form

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const mustacheExpress = require('mustache-express')
 const pgPromise = require('pg-promise')()
+const bodyParser = require('body-parser')
 
 const app = express()
 const database = pgPromise({database: 'all-ruby-books-node'})
@@ -12,6 +13,26 @@ app.use(express.static('public'))
 app.engine('mustache', mustacheExpress())
 app.set('views', './templates')
 app.set('view engine', 'mustache')
+
+app.use(bodyParser.urlencoded({extended: false}))
+
+app.get('/search', (request, response) => {
+  const title = request.query.title
+  
+  // Find all the books that have `title` in their title
+  // SELECT * FROM "books" WHERE "title" LIKE '%Rails%'
+  database.any('SELECT * FROM "books" where "title" LIKE $(pattern)',
+                { pattern: `%${title}%` })
+          .then((books) => {
+            response.render('home', { books: books })
+          })
+          .catch((error) => {
+            console.log('error', error)
+            /* Log this error to another database */
+            /* Third party apss to track errors */
+            response.render('error')            
+          })
+})
 
 app.get('/', (request, response) => {
   database.any('SELECT * FROM "books"')
