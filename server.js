2db2942013268b8304fad20fe11bb961974d304a
Gavin Stark <gavin@gstark.com>
2017-08-09 11:17:54 -0400

    Hello world from express

---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require('express')
+const mustacheExpress = require('mustache-express')
+const pgPromise = require('pg-promise')()
+
+const app = express()
+const database = pgPromise({database: 'all-ruby-books-node'})
+
+// Use the middleware (i.e plugin) for static files
+// and tell express they are in the 'public' folder
+app.use(express.static('public'))
+
+app.engine('mustache', mustacheExpress())
+app.set('views', './templates')
+app.set('view engine', 'mustache')
+
+app.get('/', (request, response) => {
+  response.send('hello-world')
+})
+
+app.listen(3000, () => {
+  console.log('Awake on port 3000')
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
